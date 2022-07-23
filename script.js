78e35892925e7f9b6ad0271438e07f67f9db5a20
mishraadityashankar <adityashankarmishra3@gmail.com>
2022-07-23 19:06:31 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const simpleGit = require("simple-git");
+const git = simpleGit.default();
+const branchName = "test";
+const commitMsg = "initial commit";
+const prMsg = "initial";
+async function main() {
+  try {
+    await git.checkoutLocalBranch(branchName);
+    const branch = await git.branch();
+    console.log(branch.current);
+    await git.add("./*");
+    await git.commit(commitMsg);
+    await git.push(["-u", "origin", branchName], () => console.log("done"));
+  } catch (errors) {
+    const status = await git.status();
+
+    if (status.conflicted.length > 0) {
+      return;
+    }
+
+    console.log(errors);
+  }
+}
+main();
